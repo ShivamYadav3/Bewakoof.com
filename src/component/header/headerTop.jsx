00ca31081583a2fded9bf117668ffa0ae6f94707
Shivam Yadav <shivamyadav93000@gmail.com>
2024-02-23 15:11:36 +0530

    icons

--- src/component/header/headerTop.jsx
+++ src/component/header/headerTop.jsx
@@ -3,9 +3,9 @@ import { BsPhone } from "react-icons/bs";
 
 const HeaderTop = () => {
   return (
-    <div className="h-7 flex justify-center items-center bg-[#eee] text-xs">
+    <div className="h-7 flex justify-center items-center bg-[#eee] text-[11px] text-[#333]">
       <div className="flex justify-between w-[85%] m-auto">
-        <div className="flex gap-6">
+        <div className="flex gap-5 font-thin">
           <div>Offers</div>
           <div>Fanbook</div>
           <div className="flex gap-1">
--- src/component/header/navbar.jsx
+++ src/component/header/navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { VscHeart, VscSearch } from "react-icons/vsc";
 import { IoBagOutline, IoSearch } from "react-icons/io5";
+import { BsPerson, BsPersonFillSlash } from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="h-12 flex justify-between items-center border-b-2 w-[85%] m-auto">
-        <div className="flex gap-6 text-sm">
+    <div className="border-b-2">
+      <div className="h-12 flex justify-between items-center w-[85%] m-auto">
+        <div className="flex gap-14 text-sm">
           <div>
             <img
               src="https://images.bewakoof.com/web/ic-desktop-bwkf-trademark-logo.svg"
@@ -14,48 +15,35 @@ const Navbar = () => {
               width="147"
             />
           </div>
-          <div className="flex gap-4">
-            <div>Men</div>
-            <div>Women</div>
-            <div>Mobile Covers</div>
+          <div className="flex gap-6">
+            <div>MEN</div>
+            <div>WOMEN</div>
+            <div>MOBILE COVERS</div>
           </div>
         </div>
         <div className="flex gap-4 items-center">
-          <div className="flex h-8 px-2 gap-3 bg-[#eee] border rounded-lg items-center">
+          <div className="flex h-10 w-72 box-content px-2 gap-3 bg-[#eee] border rounded-lg items-center">
             <div>
               <IoSearch />
             </div>
             <div>
               <input
                 type="text"
-                className="outline-none h-8 w-72 bg-[#eee] text-xs"
+                className="outline-none h-10 bg-[#eee] text-xs"
                 placeholder="Search by product, category or collection"
               />
             </div>
           </div>
-          <div className="w-0 border"></div>
+          <div className="w-0 h-6 border"></div>
+
           <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="svg-icon"
-              style={{
-                width: "1em",
-                height: "1em",
-                verticalAlign: "middle",
-                fill: "currentColor",
-                overflow: "hidden",
-              }}
-              viewBox="0 0 1024 1024"
-              version="1.1"
-            >
-              <path d="M843.282963 870.115556c-8.438519-140.515556-104.296296-257.422222-233.908148-297.14963C687.881481 536.272593 742.4 456.533333 742.4 364.088889c0-127.241481-103.158519-230.4-230.4-230.4S281.6 236.847407 281.6 364.088889c0 92.444444 54.518519 172.183704 133.12 208.877037-129.611852 39.727407-225.46963 156.634074-233.908148 297.14963-0.663704 10.903704 7.964444 20.195556 18.962963 20.195556l0 0c9.955556 0 18.299259-7.774815 18.962963-17.73037C227.745185 718.506667 355.65037 596.385185 512 596.385185s284.254815 122.121481 293.357037 276.195556c0.568889 9.955556 8.912593 17.73037 18.962963 17.73037C835.318519 890.311111 843.946667 881.019259 843.282963 870.115556zM319.525926 364.088889c0-106.287407 86.186667-192.474074 192.474074-192.474074s192.474074 86.186667 192.474074 192.474074c0 106.287407-86.186667 192.474074-192.474074 192.474074S319.525926 470.376296 319.525926 364.088889z" />
-            </svg>
+            <BsPerson className="text-2xl" />
           </div>
           <div>
-            <VscHeart />
+            <VscHeart className="text-2xl" />
           </div>
           <div>
-            <IoBagOutline />
+            <IoBagOutline className="text-2xl" />
           </div>
         </div>
       </div>
